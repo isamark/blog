7e873384606422b6af921e9b5e2942f9a24b2ec7
amark <z052256>
2019-08-26 06:56:42 +0800

    saf

--- WebApp/web/markdown.py
+++ WebApp/web/markdown.py
@@ -4,7 +4,7 @@
 # Date: 2019/8/25
 import datetime
 
-
+import logging
 from . import web
 from WebApp.forms import ArticleForm
 from flask import render_template, redirect, url_for
@@ -28,6 +28,9 @@ def test_1():
         # 文章内容以markdown的格式存储，需要显示页面时可通过markdown模块解析后显示。如
         # print(markdown.markdown(form.content.data))
         article = Article()
+        print("$$$" * 100)
+        logging.info(form.title.data)
+        logging.info("test_1执行")
         article.save_to_article(title=form.title.data, content=form.content.data, create_address=form.create_address.data, autor=form.autor.data)
 
         return redirect(url_for('web.get_article_list'))
